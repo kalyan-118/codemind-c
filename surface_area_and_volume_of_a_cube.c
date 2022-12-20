9016a2e13ea9b560ee8b91fa0489f540af8a65c8
kalyan-118 <119718641+kalyan-118@users.noreply.github.com>
2022-12-20 20:25:28 +0530

    #include<stdio.h> int main() {     int a;     int area,volume;     scanf("%d",&a);     area=6*a*a;     printf("Surface area = %d",area);     volume=a*a*a;     printf(" and Volume = %d",volume);
    }

---
+++ surface_area_and_volume_of_a_cube.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int area,volume;
+    scanf("%d",&a);
+    area=6*a*a;
+    printf("Surface area = %d",area);
+    volume=a*a*a;
+    printf(" and Volume = %d",volume);
+    
+
+}
